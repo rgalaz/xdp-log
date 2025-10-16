e36be0a486eb8f64e7fb0e1406a15fa81cca2800
Roberto Galaz <r.galaz11@gmail.com>
2025-10-16 20:37:12 -0300

    initial commit

---
+++ xdp-log/build.rs
@@ -0,0 +1,14 @@
+use anyhow::{Context as _, anyhow};
+use aya_build::{Toolchain, cargo_metadata};
+
+fn main() -> anyhow::Result<()> {
+    let cargo_metadata::Metadata { packages, .. } = cargo_metadata::MetadataCommand::new()
+        .no_deps()
+        .exec()
+        .context("MetadataCommand::exec")?;
+    let ebpf_package = packages
+        .into_iter()
+        .find(|cargo_metadata::Package { name, .. }| name.as_str() == "xdp-log-ebpf")
+        .ok_or_else(|| anyhow!("xdp-log-ebpf package not found"))?;
+    aya_build::build_ebpf([ebpf_package], Toolchain::default())
+}
---
+++ xdp-log/src/main.rs
@@ -0,0 +1,58 @@
+use anyhow::Context;
+use aya::programs::{Xdp, XdpFlags};
+use aya_log::EbpfLogger;
+use clap::Parser;
+use log::{info, warn};
+use tokio::signal;
+
+#[derive(Debug, Parser)]
+struct Opt {
+    #[clap(short, long, default_value = "enp10s0")]
+    iface: String,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let opt = Opt::parse();
+
+    env_logger::init();
+
+    // This will include your eBPF object file as raw bytes at compile-time and load it at
+    // runtime. This approach is recommended for most real-world use cases. If you would
+    // like to specify the eBPF program at runtime rather than at compile-time, you can
+    // reach for `Ebpf::load_file` instead.
+    let mut bpf = aya::Ebpf::load(aya::include_bytes_aligned!(concat!(
+        env!("OUT_DIR"),
+        "/xdp-log"
+    )))?;
+    match EbpfLogger::init(&mut bpf) {
+        Err(e) => {
+            // This can happen if you remove all log statements from your eBPF program.
+            warn!("failed to initialize eBPF logger: {e}");
+        }
+        Ok(logger) => {
+            let mut logger = tokio::io::unix::AsyncFd::with_interest(
+                logger,
+                tokio::io::Interest::READABLE,
+            )?;
+            tokio::task::spawn(async move {
+                loop {
+                    let mut guard = logger.readable_mut().await.unwrap();
+                    guard.get_inner_mut().flush();
+                    guard.clear_ready();
+                }
+            });
+        }
+    }
+    let program: &mut Xdp =
+        bpf.program_mut("xdp_firewall").unwrap().try_into()?;
+    program.load()?;
+    program.attach(&opt.iface, XdpFlags::default())
+        .context("failed to attach the XDP program with default flags - try changing XdpFlags::default() to XdpFlags::SKB_MODE")?;
+
+    info!("Waiting for Ctrl-C...");
+    signal::ctrl_c().await?;
+    info!("Exiting...");
+
+    Ok(())
+}
